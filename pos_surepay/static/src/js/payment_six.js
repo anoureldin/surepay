963595b628bde2872788e749badd5844b5b0d5b0
Ahmed Nour <a7med.nor@gmail.com>
2025-02-05 16:14:24 +0300

    so

--- pos_surepay/static/src/js/payment_six.js
+++ pos_surepay/static/src/js/payment_six.js
@@ -1,183 +1,141 @@
 /* global timapi */
-function send_Amount(Amount) 
-{
+function send_Amount(Amount) {
     try {
         var response = Android.startPayment(Amount.toString());
-
         console.log("Passed Android function");
 
-        if (response.includes("|")) 
-        {
+        if (response.includes("|")) {
             console.log("Passed Include function");
-
             var responseArray = response.split("|");
-
             console.log("Passed split function");
 
             var status = responseArray[0];
-            
-            switch (status) 
-            {
-                case "0":  
+
+            switch (status) {
+                case "0":
                     console.log("Approved");
-                    return new Promise((resolve) => {    
+                    // Store the payment data in PaymentSix instance for later use
+                    window.paymentResponseData = {
+                        cardholderName: responseArray[1], // Masked PAN
+                        cardType: responseArray[2],       // Scheme Name
+                        transactionId: responseArray[6]   // Reference No
+                    };
+                    return new Promise((resolve) => {
                         this.transactionResolve = resolve;
-                        this.transactionResolve(true); 
+                        this.transactionResolve(true);
                     });
-                 break;
-                case "1":  
+                case "1":
                     console.log("Retry");
                     return Promise.resolve();
-                break;
                 default:
                     console.warn("Invalid payment status:", status);
+                    return Promise.resolve();
             }
-        } else 
-        {
+        } else {
             console.warn("Error response :", response);
-            return Promise.resolve(); 
+            return Promise.resolve();
         }
     } catch (error) {
         console.error(error);
+        return Promise.reject(error);
     }
 }
 
-/*function paymentCallback(status) {
-      // Check if input is a string
-      if (typeof status !== 'string') {
-        console.error("Error: paymentCallback requires a string input.");
-        return;
-      }
-    }
-*/
 odoo.define('pos_surepay.payment', function (require) {
-"use strict";
+    "use strict";
 
-const { Gui } = require('point_of_sale.Gui');
-var core = require('web.core');
-var PaymentInterface = require('point_of_sale.PaymentInterface');
-const { escape } = require("@web/core/utils/strings");
-var _t = core._t;
-console.error("SUREPAY IMPLE.2212");
-console.log('oooooo');
+    const { Gui } = require('point_of_sale.Gui');
+    var core = require('web.core');
+    var PaymentInterface = require('point_of_sale.PaymentInterface');
+    const { escape } = require("@web/core/utils/strings");
+    var _t = core._t;
+    console.error("SUREPAY IMPLE.2212");
+    console.log('oooooo');
 
-
-window.onTimApiReady = function () {};
-window.onTimApiPublishLogRecord = function (record) {
-    // Log only warning or errors
-    if (record.matchesLevel(timapi.LogRecord.LogLevel.warning)) {
-        timapi.log(String(record));
-    }
-};
-
-var PaymentSix = PaymentInterface.extend({
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    /**
-     * @override
-     */
-    init: function () {
-        this._super.apply(this, arguments);
-        this.enable_reversals();
-
-        var terminal_ip = this.payment_method.six_terminal_ip;
-        var instanced_payment_method = _.find(this.pos.payment_methods, function(payment_method) {
-            return payment_method.use_payment_terminal === "six"
-                && payment_method.six_terminal_ip === terminal_ip
-                && payment_method.payment_terminal
-        })
-        if (instanced_payment_method !== undefined) {
-            var payment_terminal = instanced_payment_method.payment_terminal;
-            this.terminal = payment_terminal.terminal;
-            this.terminalListener = payment_terminal.terminalListener;
-            return;
+    window.onTimApiReady = function () {};
+    window.onTimApiPublishLogRecord = function (record) {
+        if (record.matchesLevel(timapi.LogRecord.LogLevel.warning)) {
+            timapi.log(String(record));
         }
+    };
 
-        var settings = new timapi.TerminalSettings();
-        settings.connectionMode = timapi.constants.ConnectionMode.onFixIp;
-        settings.connectionIPString = this.payment_method.six_terminal_ip;
-        settings.connectionIPPort = "80";
-        settings.integratorId = "175d97a0-2a88-4413-b920-e90037b582ac";
-        settings.dcc = false;
+    var PaymentSix = PaymentInterface.extend({
+        init: function () {
+            this._super.apply(this, arguments);
+            this.enable_reversals();
 
-        this.terminal = new timapi.Terminal(settings);
-        this.terminal.setPosId(this.pos.pos_session.name);
-        this.terminal.setUserId(this.pos.pos_session.user_id[0]);
+            var terminal_ip = this.payment_method.six_terminal_ip;
+            var instanced_payment_method = _.find(this.pos.payment_methods, function(payment_method) {
+                return payment_method.use_payment_terminal === "six"
+                    && payment_method.six_terminal_ip === terminal_ip
+                    && payment_method.payment_terminal;
+            });
 
-        this.terminalListener = new timapi.DefaultTerminalListener();
-        this.terminalListener.transactionCompleted = this._onTransactionComplete.bind(this);
-        this.terminalListener.balanceCompleted = this._onBalanceComplete.bind(this);
-        this.terminal.addListener(this.terminalListener);
-
-        var recipients = [timapi.constants.Recipient.merchant, timapi.constants.Recipient.cardholder];
-        var options = [];
-        _.forEach(recipients, (recipient) => {
-            var option = new timapi.PrintOption(
-                recipient,
-                timapi.constants.PrintFormat.normal,
-                45,
-                [timapi.constants.PrintFlag.suppressHeader, timapi.constants.PrintFlag.suppressEcrInfo]
-            );
-            options.push(option);
-        });
-        this.terminal.setPrintOptions(options);
-    },
-
-    /**
-     * @override
-     */
-    send_payment_cancel: function () {
-        this._super.apply(this, arguments);
-        this.terminal.cancel();
-        return Promise.resolve();
-    },
-    
-    /**
-     * @override
-     */
-    send_payment_request: function () {
-        var Amount =this.pos.get_order().selected_paymentline.amount*100;
-        this._super.apply(this, arguments);
-        this.pos.get_order().selected_paymentline.set_payment_status('waitingCard');
-        return send_Amount(Amount);
-    },
-
-    /**
-     * @override
-     */
-    send_payment_reversal: function () {
-        this._super.apply(this, arguments);
-        this.pos.get_order().selected_paymentline.set_payment_status('reversing');
-        return this._sendTransaction(timapi.constants.TransactionType.reversal);
-    },
-
-    send_balance: function () {
-        this.terminal.balanceAsync();
-    },
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    _onTransactionComplete: function (event, data) {
-        timapi.DefaultTerminalListener.prototype.transactionCompleted(event, data);
-        /*
-        if (event.exception) {
-            if (event.exception.resultCode !== timapi.constants.ResultCode.apiCancelEcr) {
-                Gui.showPopup('ErrorPopup', {
-                    title: _t('Transaction was not processed correctly'),
-                    body: event.exception.errorText,
-                });
+            if (instanced_payment_method !== undefined) {
+                var payment_terminal = instanced_payment_method.payment_terminal;
+                this.terminal = payment_terminal.terminal;
+                this.terminalListener = payment_terminal.terminalListener;
+                return;
             }
 
-            this.transactionResolve();
-        } else {*/
-            
-            if (data.printData){
-                this._printReceipts(data.printData.receipts)
+            var settings = new timapi.TerminalSettings();
+            settings.connectionMode = timapi.constants.ConnectionMode.onFixIp;
+            settings.connectionIPString = this.payment_method.six_terminal_ip;
+            settings.connectionIPPort = "80";
+            settings.integratorId = "175d97a0-2a88-4413-b920-e90037b582ac";
+            settings.dcc = false;
+
+            this.terminal = new timapi.Terminal(settings);
+            this.terminal.setPosId(this.pos.pos_session.name);
+            this.terminal.setUserId(this.pos.pos_session.user_id[0]);
+
+            this.terminalListener = new timapi.DefaultTerminalListener();
+            this.terminalListener.transactionCompleted = this._onTransactionComplete.bind(this);
+            this.terminalListener.balanceCompleted = this._onBalanceComplete.bind(this);
+            this.terminal.addListener(this.terminalListener);
+
+            var recipients = [timapi.constants.Recipient.merchant, timapi.constants.Recipient.cardholder];
+            var options = [];
+            _.forEach(recipients, (recipient) => {
+                var option = new timapi.PrintOption(
+                    recipient,
+                    timapi.constants.PrintFormat.normal,
+                    45,
+                    [timapi.constants.PrintFlag.suppressHeader, timapi.constants.PrintFlag.suppressEcrInfo]
+                );
+                options.push(option);
+            });
+            this.terminal.setPrintOptions(options);
+        },
+
+        send_payment_cancel: function () {
+            this._super.apply(this, arguments);
+            this.terminal.cancel();
+            return Promise.resolve();
+        },
+
+        send_payment_request: function () {
+            var Amount = this.pos.get_order().selected_paymentline.amount * 100;
+            this._super.apply(this, arguments);
+            this.pos.get_order().selected_paymentline.set_payment_status('waitingCard');
+            return send_Amount(Amount);
+        },
+
+        send_payment_reversal: function () {
+            this._super.apply(this, arguments);
+            this.pos.get_order().selected_paymentline.set_payment_status('reversing');
+            return this._sendTransaction(timapi.constants.TransactionType.reversal);
+        },
+
+        send_balance: function () {
+            this.terminal.balanceAsync();
+        },
+
+        _onTransactionComplete: function (event, data) {
+            timapi.DefaultTerminalListener.prototype.transactionCompleted(event, data);
+
+            if (data.printData) {
+                this._printReceipts(data.printData.receipts);
             }
 
             // Store Transaction Data
@@ -185,48 +143,59 @@ var PaymentSix = PaymentInterface.extend({
             transactionData.transSeq = data.transactionInformation.transSeq;
             this.terminal.setTransactionData(transactionData);
 
-            this.transactionResolve(true);
-        //}
-    },
-
-    _onBalanceComplete: function (event, data) {
-        if (event.exception) {
-            Gui.showPopup('ErrorPopup',{
-                'title': _t('Balance Failed'),
-                'body':  _t('The balance operation failed.'),
-            });
-        } else {
-            this._printReceipts(data.printData.receipts);
-        }
-    },
-
-    _printReceipts: function (receipts) {
-        _.forEach(receipts, (receipt) => {
-            if (receipt.recipient === timapi.constants.Recipient.merchant && this.pos.env.proxy.printer) {
-                this.pos.env.proxy.printer.print_receipt(
-                    "<div class='pos-receipt'><div class='pos-payment-terminal-receipt'>" +
-                        escape(receipt.value).replace(/\n/g, "<br />") +
-                    "</div></div>"
-                );
-            } else if (receipt.recipient === timapi.constants.Recipient.cardholder) {
-                this.pos.get_order().selected_paymentline.set_receipt_info(receipt.value);
+            // Set the payment response data from the Android terminal
+            if (window.paymentResponseData) {
+                const paymentLine = this.pos.get_order().selected_paymentline;
+                if (paymentLine) {
+                    paymentLine.card_type = window.paymentResponseData.cardType;
+                    paymentLine.cardholder_name = window.paymentResponseData.cardholderName;
+                    paymentLine.transaction_id = window.paymentResponseData.transactionId;
+                }
+                // Clear the temporary storage
+                window.paymentResponseData = null;
             }
-        });
-    },
 
-    _sendTransaction: function (transactionType) {
-        var amount = new timapi.Amount(
-            Math.round(this.pos.get_order().selected_paymentline.amount / this.pos.currency.rounding),
-            timapi.constants.Currency[this.pos.currency.name],
-            this.pos.currency.decimal_places
-        );
-        return new Promise((resolve) => {
-            this.transactionResolve = resolve;
-            this.terminal.transactionAsync(transactionType, amount);
-        });
-    },
-});
+            this.transactionResolve(true);
+        },
 
-return PaymentSix;
+        _onBalanceComplete: function (event, data) {
+            if (event.exception) {
+                Gui.showPopup('ErrorPopup',{
+                    'title': _t('Balance Failed'),
+                    'body':  _t('The balance operation failed.'),
+                });
+            } else {
+                this._printReceipts(data.printData.receipts);
+            }
+        },
+
+        _printReceipts: function (receipts) {
+            _.forEach(receipts, (receipt) => {
+                if (receipt.recipient === timapi.constants.Recipient.merchant && this.pos.env.proxy.printer) {
+                    this.pos.env.proxy.printer.print_receipt(
+                        "<div class='pos-receipt'><div class='pos-payment-terminal-receipt'>" +
+                            escape(receipt.value).replace(/\n/g, "<br />") +
+                        "</div></div>"
+                    );
+                } else if (receipt.recipient === timapi.constants.Recipient.cardholder) {
+                    this.pos.get_order().selected_paymentline.set_receipt_info(receipt.value);
+                }
+            });
+        },
+
+        _sendTransaction: function (transactionType) {
+            var amount = new timapi.Amount(
+                Math.round(this.pos.get_order().selected_paymentline.amount / this.pos.currency.rounding),
+                timapi.constants.Currency[this.pos.currency.name],
+                this.pos.currency.decimal_places
+            );
+            return new Promise((resolve) => {
+                this.transactionResolve = resolve;
+                this.terminal.transactionAsync(transactionType, amount);
+            });
+        },
+    });
+
+    return PaymentSix;
 
 });
